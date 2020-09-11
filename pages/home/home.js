85f9faba4382c837044c74701d437bda5d62219c
dawu.wang <dawu.wang@ihandysoft.com>
2020-09-11 08:44:45 +0800

    update

---
+++ pages/home/home.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        canIUse: wx.canIUse("button.open-type.getUserInfo")
+    },
+    onLoad: function () {
+
+    },
+    getUserInfo: function (e) {
+        console.log(e)
+    }
+})
