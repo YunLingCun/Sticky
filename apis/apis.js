7aa67018b393bc20c44099f8d97c37f0e4df569b
bytebody <bytebody@gmail.com>
2020-09-14 13:42:36 +0800

    update

---
+++ apis/apis.js
@@ -0,0 +1,130 @@
+// 获取小程序全局配置（变量、函数等）
+const app = getApp()
+// 定义网络请求API地址
+// const BaseURL = 'https://api.bytebody.com'
+const BaseURL = 'http://localhost'
+const AuthTokenName = 'authToken'
+const NetworkError = "网络错误，请检查网络"
+const HTTPCode = {
+    OK: 200,
+    BadRequest: 400,
+    Unauthorized: 401,
+}
+
+const defaultHeader = {
+    Version: "1.0.0",
+    Type: "miniprogram",
+    "X-APP-ID": "wxae0d23630594d9a0"
+}
+
+const fetch = (path, data, method, header, ...other) => {
+    header = {...header, ...defaultHeader}
+    return new Promise((resolve) => {
+        wx.request({
+            // 请求地址拼接
+            url: BaseURL + path,
+            data: data,
+            // 获取请求头配置
+            header: header,
+            method: method,
+            ...other,
+            success: (response) => {
+                resolve(response)
+            },
+            fail: (reason) => {
+                throw Error(reason.errMsg)
+            }
+        })
+    })
+}
+
+// 重构请求方式
+const authFetch = async (path, data, method, header) => {
+    let auth = wx.getStorageSync(AuthTokenName)
+    // auth 不存在或者已过期
+    if (!auth || auth.expires < new Date()) {
+        auth = await login().then(data => data).catch(reason => {
+            throw new Error(reason.errMsg)
+        })
+    }
+    header = {"Authorization": auth.token, ...header}
+    let response = await fetch(path, data, method, header)
+    // 如果服务器放回未授权则登录
+    if (response.statusCode === HTTPCode.Unauthorized) {
+        auth = await login().then(data => data).catch(reason => {
+            throw new Error(reason.errMsg)
+        })
+        response = await fetch(path, data, method, header)
+        return response
+    }
+    return response
+}
+
+const login = () => new Promise((resolve) => {
+    wx.login({
+        fail: (result) => {
+            throw Error(result.errMsg)
+        },
+        success: result => {
+            const data = {code: result.code, system: wx.getSystemInfoSync()}
+            const other = {dateType: "其他"}
+            fetch("/auth/miniprogram/token", data, "post", null, other).then(response => {
+                if (response.statusCode === HTTPCode.OK) {
+                    wx.setStorageSync(AuthTokenName, response.data)
+                    resolve(response.data)
+                } else {
+                    throw Error(response.data)
+                }
+            }).catch(reason => {
+                throw Error(reason.errMsg)
+            })
+        }
+    })
+})
+
+const userInfo = async (userInfo) => {
+    await authFetch("/user", userInfo, "put", null).then(response => {
+        if (response.statusCode === HTTPCode.OK) {
+            app.globalData.userInfo = response.data
+        } else {
+            throw  new Error(response.data)
+        }
+    }).catch(reason => {
+        throw new Error(reason.errMsg)
+    })
+}
+
+
+const APIs = {
+    Get: (path, data, header, loading) =>
+        fetch(path, data, "get", header, loading),
+    Post: (path, data, header, loading) =>
+        fetch(path, data, "post", header, loading),
+    Delete: (path, data, header, loading) =>
+        fetch(path, data, "delete", header, loading),
+    Put: (path, data, header, loading) =>
+        fetch(path, data, "put", header, loading),
+    Fetch: fetch,
+}
+
+const AuthAPIs = {
+    Get: (path, data, header, loading) =>
+        authFetch(path, data, "get", header, loading),
+    Post: (path, data, header, loading) =>
+        authFetch(path, data, "post", header, loading),
+    Delete: (path, data, header, loading) =>
+        authFetch(path, data, "delete", header, loading),
+    Put: (path, data, header, loading) =>
+        authFetch(path, data, "put", header, loading),
+    Fetch: authFetch
+}
+
+module.exports = {
+    BaseURL: BaseURL,
+    AuthTokenName: AuthTokenName,
+    AuthAPIs: AuthAPIs,
+    APIs: APIs,
+    HTTPCode: HTTPCode,
+    Login: login,
+    UserInfo: userInfo
+}
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,15 +1,35 @@
 //home.js
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
 //获取应用实例
 const app = getApp()
 
+
 Page({
     data: {
-        canIUse: wx.canIUse("button.open-type.getUserInfo")
+        canIUse: wx.canIUse("button.open-type.getUserInfo"),
+        auth: true
     },
     onLoad: function () {
-
+        this.isAuth()
     },
     getUserInfo: function (e) {
-        console.log(e)
+        if (e.detail.errMsg === 'getUserInfo:ok') {
+            this.setData({auth: true})
+            console.log(e.detail)
+        } else {
+            Toast('授权后才能使用哦😊😊');
+        }
+    },
+    isAuth: function () {
+        const that = this
+        wx.getSetting({
+            success(res) {
+                if (res.authSetting['scope.userInfo']) {
+                    that.setData({auth: true})
+                } else {
+                    that.setData({auth: false})
+                }
+            }
+        })
     }
 })
